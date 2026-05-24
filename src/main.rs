0c37e7c8666e92223640047b117d1afcc1a08cfc
Adam Young <adam@younglogic.com>
2026-05-24 11:13:26 -0400

    19 X 19 board with brown background and white piece

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,16 @@ impl WeiQiXiu{
     }
 }
 
+const BOARD_SPAN: f32 = 300.0;
+const BOARD_SIZE: u32 = 19;
+const PIECE_RADIUS: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0 );
+
+
 struct MyProgram;
 
 impl<Message> Program<Message> for MyProgram {
     type State = ();
+
 
     fn draw(
         &self,
@@ -47,25 +53,43 @@ impl<Message> Program<Message> for MyProgram {
     ) -> Vec<Geometry> {
         let mut frame = Frame::new(renderer, bounds.size());
 
-        frame.fill_rectangle(Point::ORIGIN, bounds.size(), Color::from_rgb(0.0, 0.2, 0.4));
+        frame.fill_rectangle(Point::ORIGIN, bounds.size(), Color::from_rgb(0.803, 0.666, 0.490));
 
         frame.fill(
-            &Path::circle(frame.center(), frame.width().min(frame.height()) / 4.0),
-            Color::from_rgb(0.6, 0.8, 1.0),
+            &Path::circle(frame.center(), PIECE_RADIUS),
+            Color::from_rgb(1.0, 1.0, 1.0),
         );
 
-        frame.stroke(
-            &Path::line(
-                frame.center() + Vector::new(-250.0, 100.0),
-                frame.center() + Vector::new(250.0, -100.0),
-            ),
-            Stroke {
-                style: Color::WHITE.into(),
-                width: 50.0,
-                ..Default::default()
-            },
-        );
+        let increment: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0 ) * 2.0;
 
+        for i in 0..BOARD_SIZE {
+            frame.stroke(
+                &Path::line(
+                    frame.center() + Vector::new(-BOARD_SPAN + increment * (i as f32),
+                                                  BOARD_SPAN),
+                    frame.center() + Vector::new(-BOARD_SPAN + increment * (i as f32),
+                                                 -BOARD_SPAN),
+                ),
+                Stroke {
+                    style: Color::BLACK.into(),
+                    width: 1.0,
+                    ..Default::default()
+                },
+            );
+            frame.stroke(
+                &Path::line(
+                    frame.center() + Vector::new(-BOARD_SPAN,
+                                                 -BOARD_SPAN + increment * (i as f32) ),
+                    frame.center() + Vector::new(BOARD_SPAN,
+                                                 -BOARD_SPAN + increment * (i as f32)),
+                ),
+                Stroke {
+                    style: Color::BLACK.into(),
+                    width: 1.0,
+                    ..Default::default()
+                },
+            );
+        }
         vec![frame.into_geometry()]
     }
 }
