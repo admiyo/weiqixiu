23d0718bbeac705291b8c8864c2e6d219208e4ce
Adam Young <adam@younglogic.com>
2026-05-24 11:58:56 -0400

    place black and white stones. also ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use iced::{
 };
 
 fn main() -> iced::Result {
-    iced::run( WeiQiXiu::update, WeiQiXiu::view)
+    iced::run(WeiQiXiu::update, WeiQiXiu::view)
 }
 
 #[derive(Clone, Debug)]
@@ -17,7 +17,7 @@ enum Message {}
 #[derive(Default)]
 struct WeiQiXiu;
 
-impl WeiQiXiu{
+impl WeiQiXiu {
     fn update(&mut self, _message: Message) {}
 
     fn view(&self) -> iced::Element<'_, Message> {
@@ -34,14 +34,13 @@ impl WeiQiXiu{
 
 const BOARD_SPAN: f32 = 300.0;
 const BOARD_SIZE: u32 = 19;
-const PIECE_RADIUS: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0 );
-
+const PIECE_RADIUS: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0);
+const INCREMENT: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0) * 2.0;
 
 struct MyProgram;
 
 impl<Message> Program<Message> for MyProgram {
     type State = ();
-
 
     fn draw(
         &self,
@@ -53,22 +52,17 @@ impl<Message> Program<Message> for MyProgram {
     ) -> Vec<Geometry> {
         let mut frame = Frame::new(renderer, bounds.size());
 
-        frame.fill_rectangle(Point::ORIGIN, bounds.size(), Color::from_rgb(0.803, 0.666, 0.490));
-
-        frame.fill(
-            &Path::circle(frame.center(), PIECE_RADIUS),
-            Color::from_rgb(1.0, 1.0, 1.0),
+        frame.fill_rectangle(
+            Point::ORIGIN,
+            bounds.size(),
+            Color::from_rgb(0.803, 0.666, 0.490),
         );
-
-        let increment: f32 = BOARD_SPAN / ((BOARD_SIZE as f32) - 1.0 ) * 2.0;
 
         for i in 0..BOARD_SIZE {
             frame.stroke(
                 &Path::line(
-                    frame.center() + Vector::new(-BOARD_SPAN + increment * (i as f32),
-                                                  BOARD_SPAN),
-                    frame.center() + Vector::new(-BOARD_SPAN + increment * (i as f32),
-                                                 -BOARD_SPAN),
+                    frame.center() + Vector::new(-BOARD_SPAN + INCREMENT * (i as f32), BOARD_SPAN),
+                    frame.center() + Vector::new(-BOARD_SPAN + INCREMENT * (i as f32), -BOARD_SPAN),
                 ),
                 Stroke {
                     style: Color::BLACK.into(),
@@ -78,10 +72,8 @@ impl<Message> Program<Message> for MyProgram {
             );
             frame.stroke(
                 &Path::line(
-                    frame.center() + Vector::new(-BOARD_SPAN,
-                                                 -BOARD_SPAN + increment * (i as f32) ),
-                    frame.center() + Vector::new(BOARD_SPAN,
-                                                 -BOARD_SPAN + increment * (i as f32)),
+                    frame.center() + Vector::new(-BOARD_SPAN, -BOARD_SPAN + INCREMENT * (i as f32)),
+                    frame.center() + Vector::new(BOARD_SPAN, -BOARD_SPAN + INCREMENT * (i as f32)),
                 ),
                 Stroke {
                     style: Color::BLACK.into(),
@@ -89,6 +81,23 @@ impl<Message> Program<Message> for MyProgram {
                     ..Default::default()
                 },
             );
+        }
+        let start_x: f32 = 212.0;
+        let start_y: f32 = 75.0;
+
+        const BLACK: Color = Color::from_rgb(0.0, 0.0, 0.0);
+        const WHITE: Color = Color::from_rgb(1.0, 1.0, 1.0);
+
+        for i in (0..BOARD_SIZE).step_by(2) {
+            for j in (0..BOARD_SIZE).step_by(3) {
+                let color: Color = { if i < 10 { WHITE } else { BLACK } };
+
+                let pos: Point = Point::new(
+                    start_x + INCREMENT * (i as f32),
+                    start_y + INCREMENT * (j as f32),
+                );
+                frame.fill(&Path::circle(pos, PIECE_RADIUS), color);
+            }
         }
         vec![frame.into_geometry()]
     }
